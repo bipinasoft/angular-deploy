268766df7dcf090495d85df795e5f084fa725a09
Bipina Sahoo <bipinasoft@gmail.com>
2020-04-16 22:37:45 +0530

    Element Modifications

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es6/symbol';
+/*import 'core-js/es6/symbol';
 import 'core-js/es6/object';
 import 'core-js/es6/function';
 import 'core-js/es6/parse-int';
@@ -30,7 +30,7 @@ import 'core-js/es6/date';
 import 'core-js/es6/array';
 import 'core-js/es6/regexp';
 import 'core-js/es6/map';
-import 'core-js/es6/set';
+import 'core-js/es6/set';*/
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
@@ -44,8 +44,8 @@ import 'core-js/es6/set';
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+/*import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';*/
 
 
 /**
@@ -86,4 +86,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-import 'intl';  // Run `npm install --save intl`.
+/*import 'intl';*/  // Run `npm install --save intl`.
